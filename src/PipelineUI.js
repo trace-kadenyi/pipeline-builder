cdc5d8d8c52278655c20a75edb0d3cc07292442b
Tracey Kadenyi <treykadenyi@gmail.com>
2026-06-26 17:01:22 +0300

    correct typo from wv to vw in inline styles

--- src/PipelineUI.js
+++ src/PipelineUI.js
@@ -101,7 +101,7 @@ export const PipelineUI = () => {
 
   return (
     <>
-      <div ref={reactFlowWrapper} style={{ width: "100wv", height: "70vh" }}>
+      <div ref={reactFlowWrapper} style={{ width: "100vw", height: "70vh" }}>
         <ReactFlow
           nodes={nodes}
           edges={edges}
