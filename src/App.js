3607be2ed996b4773f54cf96ed7321a034faf0d7
Tracey Kadenyi <treykadenyi@gmail.com>
2026-06-27 13:22:59 +0300

    feat: integrate Toaster provider in App
    Add react-hot-toast Toaster component with:
    - Top-right positioning for optimal visibility
    - Infinite duration for custom toast cards
    - Consistent styling with application theme

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import { PipelineToolbar } from './PipelineToolbar';
-import { PipelineUI } from './PipelineUI';
-import { SubmitButton } from './SubmitButton';
+import { Toaster } from "react-hot-toast";
+import { PipelineToolbar } from "./PipelineToolbar";
+import { PipelineUI } from "./PipelineUI";
+import { SubmitButton } from "./SubmitButton";
 
 function App() {
   return (
     <div>
+      <Toaster
+        position="top-right"
+        toastOptions={{
+          custom: {
+            duration: Infinity,
+          },
+        }}
+        containerStyle={{
+          top: 24,
+          right: 24,
+        }}
+      />
       <PipelineToolbar />
       <PipelineUI />
       <SubmitButton />
